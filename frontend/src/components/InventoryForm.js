bde8127fbd43945c824ceb98f9ca7a4bfaf97ac4
Kennyy02 <136206628+Kennyy02@users.noreply.github.com>
2025-10-25 03:21:56 +0800

    Update InventoryForm.js

--- frontend/src/components/InventoryForm.js
+++ frontend/src/components/InventoryForm.js
@@ -1079,11 +1079,11 @@ export default function InventoryForm() {
         .simple-cart-table thead th:nth-child(5) { text-align: center; }
         
         /* Column alignment and widths */
-        .simple-cart-table th:nth-child(1), .simple-cart-table td:nth-child(1) { width: 52%; text-align: left; }
+        .simple-cart-table th:nth-child(1), .simple-cart-table td:nth-child(1) { width: 60%; text-align: left; }
         .simple-cart-table th:nth-child(2), .simple-cart-table td:nth-child(2) { width: 12%; text-align: center; }
-        .simple-cart-table th:nth-child(3), .simple-cart-table td:nth-child(3) { width: 16%; text-align: right; }
-        .simple-cart-table th:nth-child(4), .simple-cart-table td:nth-child(4) { width: 16%; text-align: right; }
-        .simple-cart-table th:nth-child(5), .simple-cart-table td:nth-child(5) { width: 4%; text-align: center; }
+        .simple-cart-table th:nth-child(3), .simple-cart-table td:nth-child(3) { width: 13%; text-align: right; }
+        .simple-cart-table th:nth-child(4), .simple-cart-table td:nth-child(4) { width: 13%; text-align: right; }
+        .simple-cart-table th:nth-child(5), .simple-cart-table td:nth-child(5) { width: 2%; text-align: center; }
 
 
         /* Clamp long item names to a single line with ellipsis */
