1d4fa79eb3e403420422f3f68eb8a007ad892442
Kennyy02 <136206628+Kennyy02@users.noreply.github.com>
2025-11-03 00:08:25 +0800

    Update InventoryForm.js

--- frontend/src/components/InventoryForm.js
+++ frontend/src/components/InventoryForm.js
@@ -1082,10 +1082,10 @@ export default function InventoryForm() {
         
         /* Align table headers to match their column value alignment */
 .simple-cart-table thead th:nth-child(1) { width: 80%; text-align: left; }
-.simple-cart-table thead th:nth-child(2) { width: 28%; text-align: center; } /* QTY */
-.simple-cart-table thead th:nth-child(3) { width: 28%; text-align: right; }  /* PRICE */
-.simple-cart-table thead th:nth-child(4) { width: 28%; text-align: right; }  /* SUBTOTAL */
-.simple-cart-table thead th:nth-child(5) { width: 18%; text-align: center; }
+.simple-cart-table thead th:nth-child(2) { width: 30%; text-align: center; } /* QTY */
+.simple-cart-table thead th:nth-child(3) { width: 30%; text-align: right; }  /* PRICE */
+.simple-cart-table thead th:nth-child(4) { width: 30%; text-align: right; }  /* SUBTOTAL */
+.simple-cart-table thead th:nth-child(5) { width: 20%; text-align: center; }
 
 /* Column alignment and widths - Ensure the body cells align */
 .simple-cart-table td:nth-child(1) { width: 80%; text-align: left; }
