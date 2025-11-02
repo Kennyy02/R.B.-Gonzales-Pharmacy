c690f9201d5d7afb9d67c4ac66ea0d586d1d8bb2
Kennyy02 <136206628+Kennyy02@users.noreply.github.com>
2025-11-02 23:27:17 +0800

    Update InventoryForm.js

--- frontend/src/components/InventoryForm.js
+++ frontend/src/components/InventoryForm.js
@@ -1081,18 +1081,18 @@ export default function InventoryForm() {
         }
         
         /* Align table headers to match their column value alignment */
-.simple-cart-table thead th:nth-child(1) { width: 60%; text-align: left; }
-.simple-cart-table thead th:nth-child(2) { width: 12%; text-align: center; } /* QTY */
-.simple-cart-table thead th:nth-child(3) { width: 13%; text-align: right; }  /* PRICE */
-.simple-cart-table thead th:nth-child(4) { width: 13%; text-align: right; }  /* SUBTOTAL */
-.simple-cart-table thead th:nth-child(5) { width: 2%; text-align: center; }
+.simple-cart-table thead th:nth-child(1) { width: 80%; text-align: left; }
+.simple-cart-table thead th:nth-child(2) { width: 32%; text-align: center; } /* QTY */
+.simple-cart-table thead th:nth-child(3) { width: 33%; text-align: right; }  /* PRICE */
+.simple-cart-table thead th:nth-child(4) { width: 33%; text-align: right; }  /* SUBTOTAL */
+.simple-cart-table thead th:nth-child(5) { width: 22%; text-align: center; }
 
 /* Column alignment and widths - Ensure the body cells align */
-.simple-cart-table td:nth-child(1) { width: 60%; text-align: left; }
-.simple-cart-table td:nth-child(2) { width: 12%; text-align: center; } /* QTY */
-.simple-cart-table td:nth-child(3) { width: 13%; text-align: right; }  /* PRICE */
-.simple-cart-table td:nth-child(4) { width: 13%; text-align: right; }  /* SUBTOTAL */
-.simple-cart-table td:nth-child(5) { width: 2%; text-align: center; }
+.simple-cart-table td:nth-child(1) { width: 80%; text-align: left; }
+.simple-cart-table td:nth-child(2) { width: 32%; text-align: center; } /* QTY */
+.simple-cart-table td:nth-child(3) { width: 33%; text-align: right; }  /* PRICE */
+.simple-cart-table td:nth-child(4) { width: 33%; text-align: right; }  /* SUBTOTAL */
+.simple-cart-table td:nth-child(5) { width: 22%; text-align: center; }
 
         /* Clamp long item names to a single line with ellipsis */
         .simple-cart-table td:first-child {
